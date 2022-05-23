757028559a1e3da025f464c3a402eaa55a9197f7
hakeem durogbola <42512069+keemzyddev@users.noreply.github.com>
2022-05-23 14:10:16 +0100

    update passportjs
    change the dot then syntax to Async and await

--- config/passport.js
+++ config/passport.js
@@ -4,28 +4,33 @@ const bcrypt = require('bcryptjs');
 // Load User model
 const User = require('../models/User');
 
-module.exports = function(passport) {
+module.exports = function (passport) {
   passport.use(
-    new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-      // Match user
-      User.findOne({
-        email: email
-      }).then(user => {
-        if (!user) {
-          return done(null, false, { message: 'That email is not registered' });
-        }
+    new LocalStrategy(
+      { usernameField: "email" },
+      async (email, password, done) => {
+        try {
+          //Match user
+          const user = await User.findOne({ email: email });
+          if (!user) {
+            return done(null, false, {
+              message: "This email is not registered",
+            });
+          }
 
-        // Match password
-        bcrypt.compare(password, user.password, (err, isMatch) => {
-          if (err) throw err;
+          //Match password
+          let isMatch = await bcrypt.compare(password, user.password);
+
           if (isMatch) {
             return done(null, user);
           } else {
-            return done(null, false, { message: 'Password incorrect' });
+            return done(null, false, { message: "password incorrect" });
           }
-        });
-      });
-    })
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    )
   );
 
   passport.serializeUser(function(user, done) {
